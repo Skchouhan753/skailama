3901270de9e95257823c26f00a571f32220ec915
Subhash Kumar Chouhan <104707355+Skchouhan753@users.noreply.github.com>
2024-11-16 00:25:34 +0530

    initial commit

---
+++ skailama_frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import LoginSection from './components/loginPage/LoginSection'
+
+function App() {
+  
+
+  return (
+    <>
+    <LoginSection></LoginSection>
+    </>
+  )
+}
+
+export default App
